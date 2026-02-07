e59f40545c13abd12562ee4b86fa8bcda2873156
kitso20 <motlashupingkitso@gmail.com>
2026-02-07 21:11:46 +0200

    passed all the tests in question 2

--- data_structures.py
+++ data_structures.py
@@ -7,7 +7,7 @@ def split_coords(coordinates: list) -> tuple:
     for i in coordinates:
         ta[0].append(i[0])
         ta[1].append(i[1])
-    if len(ta[0]) == len(ta[1]):
+    if len([0]) == len(ta[1]):
         return tuple(ta)
     else:
         raise ValueError
